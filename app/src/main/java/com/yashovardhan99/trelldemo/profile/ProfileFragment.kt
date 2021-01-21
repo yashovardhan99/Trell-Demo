a19467fa77fc9741c3a8de67f42a4e329be6eb57
yashovardhan99 <yashovardhandhanania@gmail.com>
2021-01-21 16:39:46 +0530

    Added data binding for profile

--- app/src/main/java/com/yashovardhan99/trelldemo/profile/ProfileFragment.kt
+++ app/src/main/java/com/yashovardhan99/trelldemo/profile/ProfileFragment.kt
@@ -1,6 +1,19 @@
 package com.yashovardhan99.trelldemo.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.yashovardhan99.trelldemo.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
