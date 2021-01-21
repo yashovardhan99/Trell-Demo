9fdf3ae3eca5c73f1057ec36679b62288514f021
yashovardhan99 <yashovardhandhanania@gmail.com>
2021-01-22 00:22:57 +0530

    Added zoom effect for banner viewpager

--- app/src/main/java/com/yashovardhan99/trelldemo/feed/BannerAdapter.kt
+++ app/src/main/java/com/yashovardhan99/trelldemo/feed/BannerAdapter.kt
@@ -1,13 +1,16 @@
 package com.yashovardhan99.trelldemo.feed
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.yashovardhan99.trelldemo.databinding.CardBannerBinding
+import kotlin.math.abs
 
 class BannerAdapter(private val activity: FragmentActivity) :
     RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
@@ -15,6 +18,9 @@ class BannerAdapter(private val activity: FragmentActivity) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(activity: FragmentActivity) {
             binding.viewPager.adapter = BannerPageAdapter(activity)
+            binding.viewPager.clipToPadding = false
+            binding.viewPager.offscreenPageLimit = 1
+            binding.viewPager.setPageTransformer(ZoomOutPageTransformer())
             TabLayoutMediator(binding.tabLayout, binding.viewPager) { _, _ -> }.attach()
         }
     }
@@ -29,6 +35,40 @@ class BannerAdapter(private val activity: FragmentActivity) :
     }
 
     override fun getItemCount() = 1
+}
+
+class ZoomOutPageTransformer : ViewPager2.PageTransformer {
+    companion object {
+        private const val MIN_SCALE = 0.85f
+        private const val MIN_ALPHA = 0.5f
+    }
+
+    override fun transformPage(page: View, position: Float) {
+        page.apply {
+            when {
+                position < -1 -> alpha = 0f
+                position <= 1 -> {
+                    val scaleFactor = MIN_SCALE.coerceAtLeast(1 - abs(position))
+                    val vertMargin = height * (1 - scaleFactor) / 2
+                    val horzMargin = width * (1 - scaleFactor) / 2
+                    translationX = if (position < 0) {
+                        horzMargin - vertMargin / 2
+                    } else {
+                        horzMargin + vertMargin / 2
+                    }
+
+                    // Scale the page down (between MIN_SCALE and 1)
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+
+                    // Fade the page relative to its size.
+                    alpha = (MIN_ALPHA +
+                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                }
+                else -> alpha = 0f
+            }
+        }
+    }
 }
 
 class BannerPageAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
